14e0bb9f20293b688c730c8a30a68e47b1d13cfe
Xiyuan Zhao <xz2994@columbia.edu>
2022-10-05 21:56:08 +0000

    update part2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,33 +7,60 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @all_ratings = Movie.all_ratings
-    if params[:ratings].blank? && params[:commit] == "Refresh"
+    @all_ratings=Movie.all_ratings
+    
+    if params[:commit] == "Refresh"&&params[:ratings].blank?
+      #session.delete(:ratings)
       @ratings_to_show = []
+      @rating_for_sort={}
+  
     elsif params[:ratings]
-      @ratings_to_show = params[:ratings].keys
+      @ratings_to_show=params[:ratings].keys
+      @rating_for_sort=params[:ratings]
+      session[:ratings]=params[:ratings]
+      
+    elsif session[:ratings]
+      @ratings_to_show=session[:ratings].keys
+      @rating_for_sort=session[:ratings]
     else
       @ratings_to_show=@all_ratings
-    end
-
- 
-    if params[:sort]
-      if params[:sort].keys[0]=='sort_by_title'
+      
+    end 
+  
+    if params[:sorting_para]
+      if params[:sorting_para].keys[0]=='title'
         @movies = Movie.with_ratings(@ratings_to_show).order(:title)
-        @title_color = 'bg-warning'
-        @date_color = 'bg-white'
-      elsif params[:sort].keys[0]=='sort_by_release_date'
+        @Title_color = "bg-warning"
+        session[:sorting_para]={'title'=>1}
+        
+      elsif params[:sorting_para].keys[0]=='date'
         @movies = Movie.with_ratings(@ratings_to_show).order(:release_date)
-        @title_color = 'bg-white'
-        @date_color = 'bg-warning'
+        @Date_color = "bg-warning"
+        session[:sorting_para]={'date'=>1}
+      end 
+    
+    elsif session[:sorting_para]&&session[:ratings]
+      if session[:sorting_para].keys[0]=='title'
+         @movies = Movie.with_ratings(session[:ratings].keys).order(:title)
+         @Title_color = "bg-warning"
+         redirect_to action: :index, sorting_para:{'title'=>1}, ratings: session[:ratings]
+      elsif session[:sorting_para].keys[0]=='date'
+        @movies = Movie.with_ratings(session[:ratings].keys).order(:release_date)
+        @Date_color = "bg-warning"   
+        redirect_to action: :index, sorting_para:{'date'=>1}, ratings: session[:ratings]
       end
+    
+    elsif session[:ratings]
+      @movies = Movie.with_ratings(session[:ratings].keys)
+      #redirect_to action: :index, sorting_para:{'nothing'=>1}, ratings: session[:ratings]   
+        
     else
       @movies = Movie.with_ratings(@ratings_to_show)
-      @title_color = 'bg-white'
-      @date_color = 'bg-white'
-    end
-
-    
+      session.clear
+      @Date_color="bg-white"
+      @Title_color="bg-white"
+    end 
+     
   end
 
   def new
