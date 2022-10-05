0c6372e92ff7a40e26dd0dd28db778560f86045a
Xiyuan Zhao <xz2994@columbia.edu>
2022-10-05 22:23:17 +0000

    update part2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,8 +10,10 @@ class MoviesController < ApplicationController
     @all_ratings = Movie.all_ratings
     if params[:ratings].blank?
       @ratings_to_show = []
+      @saved_ratings = {}
     else
       @ratings_to_show = params[:ratings].keys
+      @saved_ratings = params[:ratings]
     end
 
 
@@ -20,11 +22,30 @@ class MoviesController < ApplicationController
         @movies = Movie.with_ratings(@ratings_to_show).order(:title)
         @title_color = 'bg-warning'
         @date_color = 'bg-white'
+        session[:sort]={'sort_by_title'=>1}
+
       elsif params[:sort].keys[0]=='sort_by_release_date'
         @movies = Movie.with_ratings(@ratings_to_show).order(:release_date)
         @title_color = 'bg-white'
         @date_color = 'bg-warning'
+        session[:sort]={'sort_by_release_date'=>1}
       end
+
+    elsif session[:sort] && session[:saved_ratings]
+      if session[:sort].keys[0]=='sort_by_release_date'
+        @movies = Movie.with_ratings(session[:saved_ratings]).order(:release_date)
+        @title_color = 'bg-white'
+        @date_color = 'bg-warning'
+        redirect_to action: :index, sort:{'title'=>1}, saved_ratings: session[:saved_ratings]
+      else
+        @movies = Movie.with_ratings(session[:saved_ratings]).order(:title)
+        @title_color = 'bg-white'
+        @date_color = 'bg-warning'
+        redirect_to action: :index, sort:{'title'=>1}, saved_ratings: session[:saved_ratings]
+      end
+
+    elsif session[:saved_ratings]
+      @movies = Movie.with_ratings(session[:saved_ratings].keys)
     else
       @movies = Movie.with_ratings(@ratings_to_show)
       @title_color = 'bg-white'
