a0738ec63a30ad18475c53b8a8e9801d28ce6f33
Xiyuan Zhao <xz2994@columbia.edu>
2022-10-05 18:19:30 +0000

    Create Movie class

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,6 +8,9 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.all
+    @all_ratings = Movie.all_ratings
+    @ratings_to_show = []
+    check_box_tag 
   end
 
   def new
